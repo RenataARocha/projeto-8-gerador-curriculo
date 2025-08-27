8c83a2dca4d1110af6cd507c88ade188dd33229b
RenataARocha <programadorawebrenatarocha@gmail.com>
2025-08-27 18:19:21 -0300

    chore: limpeza inicial, organização da estrutura e criação da pasta components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1>Gerador de Currículo Inteligente</h1>
+      <p>Vamos começar a construir o projeto</p>
+    </>
+  )
+}
+
+export default App
