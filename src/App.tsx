ee543f4fb82b7dc94e02dcddf4d304d952f69ceb
htu6n7yi <jcavalcanti008@gmail.com>
2025-08-30 17:26:31 -0300

    estruturação do preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import './index.css'
+import "./index.css";
+import { useState } from "react";
 import { DadosPessoaisForm } from "./components/Form/DadosPessoaisForm";
 import Preview from "./components/Preview/Preview";
+import { type DadosPessoais } from "./components/types/types";
 
 function App() {
+  const [dados, setDados] = useState<DadosPessoais>({
+    nome: "",
+    email: "",
+    telefone: "",
+    linkedin: "",
+    resumo: "",
+  });
+
   return (
-    <>
+    <div className="app">
       <h1>Gerador de Currículo Inteligente</h1>
       <p>Vamos começar a construir o projeto</p>
-      <DadosPessoaisForm />
-      <Preview />
-    </>
-  )
+
+      <DadosPessoaisForm dados={dados} setDados={setDados} />
+      <Preview dados={dados} />
+
+      <h1 className="text-3xl font-bold text-red-500">Testando Tailwind</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/types/types.ts
@@ -0,0 +1,8 @@
+// src/types.ts
+export interface DadosPessoais {
+  nome: string;
+  email: string;
+  telefone: string;
+  linkedin: string;
+  resumo: string;
+}
