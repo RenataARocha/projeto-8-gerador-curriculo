b606bc3b96922eca3553980c9e6f55fd4c90210f
htu6n7yi <jcavalcanti008@gmail.com>
2025-08-31 15:53:16 -0300

    instalação do tailwindcss

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -6,13 +6,26 @@ type Props = {
 
 export default function Preview({ dados }: Props) {
   return (
-    <div>
-      <h2>Prévia do Currículo</h2>
-      <p><strong>Nome:</strong> {dados.nome}</p>
-      <p><strong>Email:</strong> {dados.email}</p>
-      <p><strong>Telefone:</strong> {dados.telefone}</p>
-      <p><strong>LinkedIn:</strong> {dados.linkedin}</p>
-      <p><strong>Resumo:</strong> {dados.resumo}</p>
-    </div>
+    <>
+    <h2>Prévia do Currículo</h2>
+      <div className="border border-black mt-0 mr-4 p-8 min-h-[90vh]">
+        <h2 className="font-bold text-center">Dados Pessoais</h2>
+        <p>
+          <strong>Nome:</strong> {dados.nome}
+        </p>
+        <p>
+          <strong>Email:</strong> {dados.email}
+        </p>
+        <p>
+          <strong>Telefone:</strong> {dados.telefone}
+        </p>
+        <p>
+          <strong>LinkedIn:</strong> {dados.linkedin}
+        </p>
+        <p>
+          <strong>Resumo:</strong> {dados.resumo}
+        </p>
+      </div>
+    </>
   );
 }
