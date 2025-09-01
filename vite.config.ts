b858bed74501883d9b321629ff37788ad4768dd4
RenataARocha <programadorawebrenatarocha@gmail.com>
2025-09-01 11:07:54 -0300

    Ajusta base path do Vite para deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/projeto-8-gerador-curriculo/', 
   plugins: [react()],
-  base: '/projeto-8-gerador-curriculo/',
 })
