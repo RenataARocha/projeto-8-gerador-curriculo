bc1f1a34e9c8ef72c1fb875916a4dfe5d1ee7215
RenataARocha <programadorawebrenatarocha@gmail.com>
2025-09-05 21:04:52 -0300

    fix: Corrige erro de build no Vercel

--- src/components/components.ListExperiencia/ListaExperiencia.tsx
+++ src/components/components.ListExperiencia/ListaExperiencia.tsx
@@ -1,4 +1,4 @@
-import React, { useState, forwardRef, useImperativeHandle } from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
 import styles from "./ListaExperiencia.module.css";
 import type { Experiencia } from "../types/types";
 
